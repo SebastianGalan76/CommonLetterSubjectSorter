f1eeca9444f5b44175d5c44fa48a37a4013497ac
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-12-08 10:22:09 +0100

    Created data models

---
+++ src/main/java/org/example/data/Subject.java
@@ -0,0 +1,4 @@
+package org.example.data;
+
+public record Subject(String name, String instructor) {
+}
---
+++ src/main/java/org/example/data/SubjectResult.java
@@ -0,0 +1,4 @@
+package org.example.data;
+
+public record SubjectResult(Subject subject, int commonLetters) {
+}
